eb54f961787b1a0aceeb418f14667fdf37727699
Ze <jjailtonlima@gmail.com>
2022-11-06 09:40:00 -0300

    ajustando email

--- ncComum/SendMailService.vb
+++ ncComum/SendMailService.vb
@@ -20,7 +20,7 @@ Namespace nsEmail
 
         Public Sub Send()
             Dim smtp = New SmtpClient With {
-                .Host = "mail.nascom.com.br",
+                .Host = "smtp.gmail.com",
                 .Port = 587,
                 .DeliveryMethod = SmtpDeliveryMethod.Network,
                 .EnableSsl = True,
