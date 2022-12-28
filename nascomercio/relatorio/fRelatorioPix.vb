aa2e0f029854203a652c34e0c5db55b9f03eb06c
Ze <jjailtonlima@gmail.com>
2022-12-28 09:53:44 -0300

    ajustes em relatório

--- nascomercio/relatorio/fRelatorioPix.vb
+++ nascomercio/relatorio/fRelatorioPix.vb
@@ -1,7 +1,6 @@
 Imports ncRegras.nsFabricante
 Imports ncDados.nsFabricante
 Imports ncComum.nsExcecao
-Imports System.Data.SqlClient
 
 Public Class fRelatorioPix
 
@@ -43,15 +42,10 @@ Public Class fRelatorioPix
     End Sub
 
     Private Sub fFabricanteLista_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim dtb As New DataTable
-        Dim dad As SqlDataAdapter
+
 
         Try
-            'cnn.Open()
-            'dad = New SqlDataAdapter(strSql, cnn)
-            'dad.Fill(dtb)
-            'cnn.Close()
-            'dad.Dispose()
+
             Me.v_fechamentoTableAdapter.Fill(Me.nascomercioDataSet.v_fechamento)
 
         Catch nex As ExcecaoNascomercio
