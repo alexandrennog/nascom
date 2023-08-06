56f9a84130932b08368bbc54dcda32c132797d46
llope <jjailtonlima@gmail.com>
2023-08-06 10:28:06 -0300

    validando cnpj

--- nascomercio/formulario/fPagamento.vb
+++ nascomercio/formulario/fPagamento.vb
@@ -561,10 +561,15 @@ Public Class fPagamento
                     ' ECF - Impressora Fiscal
 
                     'se não informou cpf pergunta
-                    If String.IsNullOrEmpty(Str_CPF) Then
+                    If String.IsNullOrEmpty(Str_CPF) And Len(Str_CPF) = 11 Then
                         Str_CPF = InputBox("Deseja informar o CPF ?").Trim()
                         Do While Not ValidaCpf(Str_CPF)
                             Str_CPF = InputBox("CPF Incorreto, informe novamente ?").Trim()
+                        Loop
+                    ElseIf String.IsNullOrEmpty(Str_CPF) And Len(Str_CPF) = 14 Then
+                        Str_CPF = InputBox("Deseja informar o CNPJ ?").Trim()
+                        Do While Not ValidaCnpj(Str_CPF)
+                            Str_CPF = InputBox("CNPJ Incorreto, informe novamente ?").Trim()
                         Loop
 
                     End If
--- ncComum/funcoes.vb
+++ ncComum/funcoes.vb
@@ -1269,6 +1269,86 @@ Namespace nsFuncoes
 
         End Function
 
+
+        Public Shared Function ValidaCnpj(ByVal strCNPJ As String) As Boolean
+
+            ValidaCnpj = False
+
+            Dim a, j, d1, d2 As Double
+
+            Dim i As Integer
+
+            If Len(strCNPJ) <> 14 Then
+
+                ValidaCnpj = False
+
+                Exit Function
+
+            End If
+
+
+            If Not IsNumeric(strCNPJ) Then
+
+                ValidaCnpj = False
+
+                Exit Function
+
+            End If
+
+
+
+            a = 0
+
+            i = 0
+
+            d1 = 0
+
+            d2 = 0
+
+            j = 5
+
+            For i = 1 To 12 Step 1
+
+                a = a + (Val(Mid(strCNPJ, i, 1)) * j)
+
+                j = Convert.ToDouble(IIf(j > 2, j - 1, 9))
+
+            Next i
+
+            a = a Mod 11
+
+            d1 = Convert.ToDouble(IIf(a > 1, 11 - a, 0))
+
+            a = 0
+
+            i = 0
+
+            j = 6
+
+            For i = 1 To 13 Step 1
+
+                a = a + (Val(Mid(strCNPJ, i, 1)) * j)
+
+                j = Convert.ToDouble(IIf(j > 2, j - 1, 9))
+
+            Next i
+
+            a = a Mod 11
+
+            d2 = Convert.ToDouble(IIf(a > 1, 11 - a, 0))
+
+            If (d1 = Val(Mid(strCNPJ, 13, 1)) And d2 = Val(Mid(strCNPJ, 14, 1))) Then
+
+                ValidaCnpj = True
+
+            Else
+
+                ValidaCnpj = False
+
+            End If
+
+        End Function
+
     End Class
 
 End Namespace
