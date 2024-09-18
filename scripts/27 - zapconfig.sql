f15ae39f5114ec2c00141eed9d237f4467de3ca4
jailton <jjailtonlima@gmail.com>
2024-09-18 06:14:35 -0300

    incluindo msg zap

---
+++ scripts/27 - zapconfig.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `zapconfig` (
+  `chave` varchar(250) DEFAULT NULL,
+  `Client_id` varchar(30) DEFAULT NULL,
+  `client_secret` varchar(50) DEFAULT NULL,
+  `DataRenovacao` datetime DEFAULT NULL  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
