a8e712e58afed789d2fe2d5f9c2c931050ab1281
alexandre <alexandre.nog@gmail.com>
2025-08-17 15:56:12 -0300

    Ajuste tela vendas vendedor

---
+++ scripts/29 - sp_recuperavendas.sql
@@ -0,0 +1,28 @@
+﻿DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `sp_recuperavendas` $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_recuperavendas`(IN nomevendedor varchar(30), IN dtIni datetime, IN dtFim datetime)
+BEGIN
+
+DROP TEMPORARY TABLE IF EXISTS vendas_range;
+CREATE TEMPORARY TABLE vendas_range
+AS
+
+SELECT vendedor, (SELECT count(*)
+FROM nascomercio.vendas
+where data >= dtIni and data <= dtFim and valorvenda>0
+and vendedor = v.vendedor) as totalvendas, Sum(valorvenda) as valor, fu_getqtdprods(vendedor, dtIni, dtFim) as qtdprod
+FROM nascomercio.v_vendassintetico as v
+Where data >= dtIni and data <= dtFim
+and vendedor = ifnull(nomevendedor, vendedor)
+group by vendedor;
+
+select vendedor, COALESCE(totalvendas,0) as totalvendas, valor, qtdprod ,valor/(totalvendas) as ticket ,  qtdprod/(totalvendas) as pa
+from vendas_range
+where vendedor = ifnull(nomevendedor, vendedor)
+and totalvendas > 0
+order by valor desc;
+
+END $$
+
+DELIMITER ;
